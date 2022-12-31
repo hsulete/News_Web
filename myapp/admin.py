d6f02159e27ac3467adf4a4e6883aa7b0caf6ee7
hsulete <hsulete@hotmail.com>
2022-12-31 13:03:12 +0800

    新稱首頁news.html and 新增資料表NewsData

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from myapp.models import NewsData
+
+admin.site.register(NewsData)
 
 # Register your models here.
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from myapp import models
+import math
+page = 1
+def news(request, pageindex=None):
+    global page
+    pagesize = 8
+    newsall = models.NewsData.objects.all().order_by('-id')
+    datasize = len(newsall)
+    total_page =math.ceil(datasize/pagesize)
+    if pageindex==None:
+        page = 1
+        news_units =models.NewsData.objects.all().order_by('-id')
+
+
+
+    return render(request, "news.html" ,locals())
+
 
 # Create your views here.
